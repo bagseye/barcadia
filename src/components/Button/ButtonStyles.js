32694aee21f192639ae89d0153a951127afcdc93
Morgan Baker <morganbakerdesigner@gmail.com>
2021-04-21 23:17:39 +0100

    Completed basic button component

--- src/components/Button/ButtonStyles.js
+++ src/components/Button/ButtonStyles.js
@@ -2,18 +2,41 @@ import styled from "styled-components"
 
 export const ButtonStyles = styled.button`
   position: relative;
+  display: inline-flex;
+  align-items: center;
   background-color: transparent;
   color: #fff;
   outline: none;
   border: none;
   padding: 0;
-  height: 32px;
+  height: 36px;
+  line-height: 36px;
   cursor: pointer;
   font-weight: 700;
-  font-size: 18px;
+  font-size: 15px;
   font-family: "Heebo", sans-serif;
   letter-spacing: -0.5px;
   text-decoration: none;
+
+  svg {
+    font-size: 18px;
+  }
+
+  @media (min-width: 375px) {
+    font-size: 16px;
+  }
+
+  @media (min-width: 414px) {
+    font-size: 18px;
+
+    svg {
+      font-size: 20px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    font-size: 19px;
+  }
 
   &:after {
     content: "";
